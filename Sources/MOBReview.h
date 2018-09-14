294bd3e474d01115830f5eba26c20835ad5a2535
Jason Morcos <jason.morcos@me.com>
2018-09-13 22:50:40 -0400

    Changes for XCode 10

---
+++ Sources/MOBReview.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+//! Project version number for MOBReview.
+FOUNDATION_EXPORT double MOBReviewVersionNumber;
+
+//! Project version string for MOBReview.
+FOUNDATION_EXPORT const unsigned char MOBReviewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kanna/PublicHeader.h>
+//#import <GoogleMobileAds/GoogleMobileAds.h>
