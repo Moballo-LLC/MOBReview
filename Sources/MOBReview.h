49154c81bf3d88c225b36756f38e72147ffc2fa1
Jason Morcos <jason.morcos@me.com>
2018-11-11 12:42:04 -0500

    Cleanup

--- Sources/MOBReview.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-
-//! Project version number for MOBReview.
-FOUNDATION_EXPORT double MOBReviewVersionNumber;
-
-//! Project version string for MOBReview.
-FOUNDATION_EXPORT const unsigned char MOBReviewVersionString[];
